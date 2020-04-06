34d6029fabf1ecabf4912f90b84cc433a114336f
ishayMaya <ssaymama@gmail.com>
2020-04-06 17:46:44 +0300

    Updates

---
+++ precache-manifest.b34b67b5ece88c7d2e0361e777182005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797c06eb41685d539e8980b2ddb22de4",
+    "url": "/ovid19-confirmed/index.html"
+  },
+  {
+    "revision": "4c28886d8dd062743d89",
+    "url": "/ovid19-confirmed/static/css/main.3c705231.chunk.css"
+  },
+  {
+    "revision": "8426f89b036533769fdc",
+    "url": "/ovid19-confirmed/static/js/2.d8877178.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ovid19-confirmed/static/js/2.d8877178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c28886d8dd062743d89",
+    "url": "/ovid19-confirmed/static/js/main.d92ea523.chunk.js"
+  },
+  {
+    "revision": "3c0f32f8baf40a317c34",
+    "url": "/ovid19-confirmed/static/js/runtime-main.78e7420a.js"
+  }
+]);
